69bc7183fb6393e168216ea65e20957065764357
aaronysl <ysl158233@163.com>
2018-10-27 19:24:57 +0800

    获取排行列表

--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -1,19 +1,14 @@
 from info import sr
-from info.models import User
+from info.models import User, News
 from info.modules.home import home_blu
-import logging      #python 内置日志模块，将日志信息在控制台输出，并且可以将日志保存在文件中
-#flask 中的默认日志也是集成的logging模块，但是没有将日志保存在文件中
+import logging  # python内置的日志模块  将日志信息在控制台中输出, 并且可以将日志保存到文件中
+# flask中的默认日志也是集成的logging模块, 但是没有将日志保存到文件中
+from flask import current_app, render_template, session
 
-from flask import current_app, render_template, session  # 在其他文件中使用app    导入render 后端模版渲染
 
-
-#2.使用蓝图注册路由
+# 2.使用蓝图注册路由
 @home_blu.route('/')
 def index():
-
-    #logging.error('error found')    # logging 默认的输出不包含错误位置，显示效果不好，可以使用flask内置的日志输出语法来代替
-
-    # current_app.logger.error('error found')
     # 在根路由中判断用户是否登录
     user_id = session.get("user_id")
 
@@ -25,18 +20,25 @@ def index():
         except BaseException as e:
             current_app.logger.error(e)
 
+    # 按照`点击量`查询`前10条`新闻数据
+    news_list = []
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(10).all()
+    except BaseException as e:
+        current_app.logger.error(e)
+
+    news_list = [news.to_dict() for news in news_list]
+
     # 将模型转为字典
     user = user.to_dict() if user else None
 
     # TODO 将用户信息传入模板渲染
-    return render_template("index.html", user=user)
-
-
+    return render_template("index.html", user=user, news_list=news_list)
 
 
 
 @home_blu.route('/favicon.ico')
 def favicon():
-    #网站小图标的请求是有浏览器发起的，只需要实现该路由并返回图标对应图片即可
-    #flask中内置了返回静态文件的语法send_static_file，flask 访问静态文件的路由底层也是调用该方法
-    return current_app.send_static_file('news/favicon.ico')
+    # 网站小图标的请求是由浏览器自动发起的, 只需要实现该路由并返回图标对应图片即可
+    # flask中内置了获取静态文件的语法send_static_file, flask访问静态文件的路由底层也是调用该方法
+    return current_app.send_static_file("news/favicon.ico")
