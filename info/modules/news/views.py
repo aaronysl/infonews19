a03f8956aa20d1fa813fe4fb25a03bfcc03e93e6
aaronysl <ysl158233@163.com>
2018-10-28 19:15:35 +0800

    显示其他数据

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,11 +1,15 @@
-from flask import render_template, current_app, abort
+from flask import render_template, current_app, abort, session, g
 
-from info.models import News
+from info.models import News, User
 from info.modules.news import news_blu
 
 
 # 新闻详情
+from info.utils.common import user_login_data
+
+
 @news_blu.route('/<int:news_id>')  # 路由变量接收新闻id
+@user_login_data  # 装饰器形式来封装用户信息的查询  news_detail = user_login_data(news_detail)
 def news_detail(news_id):
     # 根据新闻id查询该新闻数据
     try:
@@ -14,5 +18,17 @@ def news_detail(news_id):
         current_app.logger.error(e)
         abort(500)
 
+    # 按照`点击量`查询`前10条`新闻数据
+    news_list = []
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(10).all()
+    except BaseException as e:
+        current_app.logger.error(e)
+
+    news_list = [news.to_dict() for news in news_list]
+
+    # 将模型转为字典
+    user = g.user.to_dict() if g.user else None
+
     # 将数据传入模板渲染
-    return render_template("detail.html", news=news.to_dict())
+    return render_template("detail.html", news=news.to_dict(), news_list=news_list, user=user)
--- info/utils/common.py
+++ info/utils/common.py
@@ -1,6 +1,37 @@
 # 过滤器的本质: 函数   1.必须设置参数接收模板变量  2> 将转换后的结果返回
+import functools
+
+from flask import session, current_app, g
+
+from info.models import User
+
+
 def func_index_convert(index):
 
     index_dict = {1: "first", 2: "second", 3: "third"}
 
     return index_dict.get(index, "")
+
+
+
+# 获取用户的登录信息
+def user_login_data(f):  # f = news_detail
+
+    @functools.wraps(f)  # 该装饰器会让闭包函数使用指定函数的信息(如函数名, 函数注释)
+    def wrapper(*args, **kwargs):
+        # 判断用户是否登录
+        user_id = session.get("user_id")
+
+        user = None  # type: User
+        if user_id:  # 已登录
+            # 查询用户信息
+            try:
+                user = User.query.get(user_id)
+            except BaseException as e:
+                current_app.logger.error(e)
+
+        g.user = user
+
+        return f(*args, **kwargs)
+
+    return wrapper
