600ad9b1f8b2a378db545c3eb8eac51ef0ce6cd0
aaronysl <ysl158233@163.com>
2018-10-30 19:39:06 +0800

    显示和修改基本资料

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -1,10 +1,12 @@
-from flask import render_template, g, redirect, url_for
+from flask import render_template, g, redirect, url_for, abort, request, jsonify
 
 from info.modules.user import user_blu
 from info.utils.common import user_login_data
 
-
 # 个人中心
+from info.utils.response_code import RET, error_map
+
+
 @user_blu.route('/user_info')
 @user_login_data
 def user_info():
@@ -16,7 +18,31 @@ def user_info():
 
 
 # 基本资料
-@user_blu.route('/base_info')
+@user_blu.route('/base_info', methods=['GET', "POST"])
+@user_login_data
 def base_info():
+    user = g.user
+    if not user:
+        return abort(403)  # 拒绝访问
 
-    return render_template("user_base_info.html")
+    if request.method == 'GET':
+        return render_template("user_base_info.html", user=user.to_dict())
+    # POST处理
+    signature = request.json.get("signature")
+    nick_name = request.json.get("nick_name")
+    gender = request.json.get("gender")
+
+    # 校验参数
+    if not all([signature, nick_name, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    if gender not in ["MAN", "WOMAN"]:
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+
+    # 修改用户模型
+    user.signature = signature
+    user.nick_name = nick_name
+    user.gender = gender
+
+    # json返回  需要传入用户信息, 以便前端进行渲染
+    return jsonify(errno=RET.OK, errmsg=error_map[RET.OK], data=user.to_dict())
