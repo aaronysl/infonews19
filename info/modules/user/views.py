89f737c486dfc03fe4b4e665541f1128342be6c3
aaronysl <ysl158233@163.com>
2018-10-30 19:01:16 +0800

    实现个人中心

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -1,9 +1,22 @@
-from flask import render_template
+from flask import render_template, g, redirect, url_for
 
 from info.modules.user import user_blu
+from info.utils.common import user_login_data
 
-#个人中心
+
+# 个人中心
 @user_blu.route('/user_info')
+@user_login_data
 def user_info():
+    user = g.user
+    if not user:  # 用户未登录, 直接跳转到首页
+        return redirect(url_for("home.index"))
 
-    return render_template("user.html")
+    return render_template("user.html", user=user.to_dict())
+
+
+# 基本资料
+@user_blu.route('/base_info')
+def base_info():
+
+    return render_template("user_base_info.html")
